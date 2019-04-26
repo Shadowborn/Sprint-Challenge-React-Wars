5d78502c5734e010bf3ae25fc006ed0fa4c8943f
Shadowborn <james.starks023@gmail.com>
2019-04-26 10:29:49 -0600

    Finished, working on stretch goals. Going to get planet API

--- starwars/src/components/CharacterItem.js
+++ starwars/src/components/CharacterItem.js
@@ -1,8 +1,6 @@
 import React from "react";
 
 function CharacterItem(props) {
-  // this constructs the className attribute for the div below conditional
-  // on whether this item's props.item.purchased is true or false
   let classNames = "item";
   
 
